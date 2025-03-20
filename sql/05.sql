dc3bdee5cb6c32ff9592f635a0e7dd17e2d1d81f
Eduardo-Mellado013 <eduardom71891@gmail.com>
2025-03-20 13:34:32 -0700

    Solved SQL Problems

--- sql/05.sql
+++ sql/05.sql
@@ -6,3 +6,14 @@
  * HINT:
  * This can be solved with a self join on the film_actor table.
  */
+
+SELECT film.title
+FROM film
+JOIN film_actor USING (film_id)
+WHERE film_actor.actor_id IN (
+    SELECT film_actor.actor_id
+    FROM film_actor
+    JOIN film USING (film_id)
+    WHERE film.title = 'AMERICAN CIRCUS'
+)
+ORDER BY title ASC;
--- sql/05b.sql
+++ sql/05b.sql
@@ -1,3 +1,17 @@
 /*
  * Write a SQL query that lists the title of all movies where at least 2 actors were also in 'AMERICAN CIRCUS'.
  */
+
+SELECT film.title
+FROM film
+JOIN film_actor USING (film_id)
+WHERE film_actor.actor_id IN (
+    SELECT film_actor.actor_id
+    FROM film_actor
+    JOIN film USING (film_id)
+    WHERE film.title = 'AMERICAN CIRCUS'
+)
+GROUP BY film.title
+HAVING COUNT(actor_id) >= 2
+ORDER BY title ASC;
+
