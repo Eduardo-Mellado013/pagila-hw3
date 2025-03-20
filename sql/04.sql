e7cc5b26ce869ab4b5e17ab45b5cb853fce1a0c3
Eduardo-Mellado013 <eduardom71891@gmail.com>
2025-03-19 21:48:28 -0700

    Solved SQL Problems

--- sql/04.sql
+++ sql/04.sql
@@ -3,3 +3,17 @@
  * 1. have appeared in at least one movie in the "Children" category,
  * 2. but that have never appeared in any movie in the "Horror" category.
  */
+
+SELECT actor.first_name, actor.last_name
+FROM actor
+JOIN film_actor USING (actor_id)
+JOIN film USING (film_id)
+JOIN film_category USING (film_id)
+JOIN category USING (category_id)
+GROUP BY actor.actor_id, actor.first_name, actor.last_name
+HAVING 
+COUNT(CASE WHEN category.name = 'Children' THEN 1 END) > 0
+AND 
+COUNT(CASE WHEN category.name = 'Horror' THEN 1 END) = 0
+ORDER BY actor.last_name, actor.first_name;
+
