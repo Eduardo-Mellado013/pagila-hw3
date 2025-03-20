29a0157487c7598ea8c505b541f631e1b941a811
Eduardo-Mellado013 <eduardom71891@gmail.com>
2025-03-20 16:26:49 -0700

    Solved SQL Problems

--- sql/05c.sql
+++ sql/05c.sql
@@ -5,3 +5,17 @@
  * Write a SQL query that lists all movies where at least 3 actors were in one of the above three movies.
  * (The actors do not necessarily have to all be in the same movie, and you do not necessarily need one actor from each movie.)
  */
+
+SELECT title
+FROM film
+JOIN film_actor USING (film_id)
+WHERE actor_id IN (
+    SELECT actor_id
+    FROM film
+    JOIN film_actor USING (film_id)
+    WHERE film.title IN ('AMERICAN CIRCUS', 'AGENT TRUMAN', 'ACADEMY DINOSAUR')
+)
+GROUP BY title
+HAVING COUNT(actor_id) > 2
+ORDER BY title;
+
--- sql/06.sql
+++ sql/06.sql
@@ -6,3 +6,19 @@
  * That is, list all actors that have appeared in a film with 'RUSSELL BACALL'.
  * Do not list 'RUSSELL BACALL', since he has a Bacall Number of 0.
  */
+
+SELECT DISTINCT actor.first_name || ' ' || actor.last_name AS "Actor Name"
+FROM film
+JOIN film_actor USING (film_id)
+JOIN actor USING (actor_id)
+WHERE film_id IN (
+    SELECT film_id
+    FROM film
+    JOIN film_actor USING (film_id)
+    JOIN actor USING (actor_id)
+    WHERE first_name = 'RUSSELL' 
+      AND last_name = 'BACALL'
+)
+AND NOT (first_name = 'RUSSELL' AND last_name = 'BACALL')
+ORDER BY "Actor Name" ASC;
+
