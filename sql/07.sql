f7c424a233c9539755829badd6a5c8d3c251a5ca
Eduardo-Mellado013 <eduardom71891@gmail.com>
2025-03-21 16:38:10 -0700

    Solved SQL Problems

--- sql/07.sql
+++ sql/07.sql
@@ -3,3 +3,46 @@
  * That is, list all actors that have appeared in a film with an actor that has appeared in a film with 'RUSSELL BACALL',
  * but do not include actors that have Bacall Number < 2.
  */
+
+SELECT DISTINCT first_name || ' ' || last_name AS "Actor Name"
+FROM film
+JOIN film_actor USING (film_id)
+JOIN actor USING (actor_id)
+WHERE film_id IN (
+    SELECT film_id
+    FROM film
+    JOIN film_actor USING (film_id)
+    WHERE actor_id IN (
+        SELECT DISTINCT actor_id
+        FROM film
+        JOIN film_actor USING (film_id)
+        JOIN actor USING (actor_id)
+        WHERE film_id IN (
+            SELECT film_id
+            FROM film
+            JOIN film_actor USING (film_id)
+            JOIN actor USING (actor_id)
+            WHERE first_name = 'RUSSELL'
+              AND last_name = 'BACALL'
+        )
+        AND NOT (first_name = 'RUSSELL' AND last_name = 'BACALL')
+    )
+)
+AND actor_id NOT IN (
+    SELECT DISTINCT actor_id
+    FROM film
+    JOIN film_actor USING (film_id)
+    JOIN actor USING (actor_id)
+    WHERE film_id IN (
+        SELECT film_id
+        FROM film
+        JOIN film_actor USING (film_id)
+        JOIN actor USING (actor_id)
+        WHERE first_name = 'RUSSELL'
+          AND last_name = 'BACALL'
+    )
+    AND NOT (first_name = 'RUSSELL' AND last_name = 'BACALL')
+)
+AND NOT (first_name = 'RUSSELL' AND last_name = 'BACALL')
+ORDER BY "Actor Name" ASC;
+
