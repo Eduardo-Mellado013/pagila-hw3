d3b3bc86dcc7cd2c79ecdab60d49cbb1d40666f7
Eduardo-Mellado013 <eduardom71891@gmail.com>
2025-03-21 22:38:25 -0700

    Solves SQL Problems

--- sql/05d.sql
+++ sql/05d.sql
@@ -12,3 +12,29 @@
  * There are many ways to solve this problem,
  * but I personally found the INTERSECT operator to make a convenient solution.
  */
+
+SELECT DISTINCT f.title
+FROM film f
+JOIN film_actor fa_ac ON f.film_id = fa_ac.film_id
+JOIN film_actor fa_ad ON f.film_id = fa_ad.film_id
+JOIN film_actor fa_at ON f.film_id = fa_at.film_id
+WHERE fa_ac.actor_id IN (
+    SELECT actor_id
+    FROM film
+    JOIN film_actor USING (film_id)
+    WHERE title = 'AMERICAN CIRCUS'
+)
+  AND fa_ad.actor_id IN (
+    SELECT actor_id
+    FROM film
+    JOIN film_actor USING (film_id)
+    WHERE title = 'ACADEMY DINOSAUR'
+)
+  AND fa_at.actor_id IN (
+    SELECT actor_id
+    FROM film
+    JOIN film_actor USING (film_id)
+    WHERE title = 'AGENT TRUMAN'
+)
+ORDER BY f.title;
+
--- sql/05f.sql
+++ sql/05f.sql
@@ -7,3 +7,30 @@
  * It's possible to complete this problem both with and without set operations,
  * but I find the version using set operations much more intuitive.
  */
+
+SELECT f.title
+FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+WHERE c.name IN (
+    SELECT c2.name
+    FROM film f2
+    JOIN film_category fc2 ON f2.film_id = fc2.film_id
+    JOIN category c2 ON fc2.category_id = c2.category_id
+    WHERE f2.title = 'AMERICAN CIRCUS'
+)
+  AND EXISTS (
+    SELECT 1
+    FROM film_actor fa
+    WHERE fa.film_id = f.film_id
+      AND fa.actor_id IN (
+          SELECT fa2.actor_id
+          FROM film_actor fa2
+          JOIN film f3 ON fa2.film_id = f3.film_id
+          WHERE f3.title = 'AMERICAN CIRCUS'
+      )
+  )
+GROUP BY f.title
+HAVING COUNT(DISTINCT c.name) >= 2
+ORDER BY f.title ASC;
+
