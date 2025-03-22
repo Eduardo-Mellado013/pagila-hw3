b5ff74135b9cb7038ffb9761ad30ed4dd2e97213
Eduardo-Mellado013 <eduardom71891@gmail.com>
2025-03-21 22:16:50 -0700

    Solves SQL Problems

--- sql/15.sql
+++ sql/15.sql
@@ -8,3 +8,19 @@
  * But there is also a view in the database that contains the correct formatting,
  * and you can SELECT from that VIEW instead of constructing the entire query manually.
  */
+
+SELECT film.title,
+       STRING_AGG(
+         upper(substring(actor.first_name from 1 for 1)) || lower(substring(actor.first_name from 2)) ||
+         upper(substring(actor.last_name from 1 for 1)) || lower(substring(actor.last_name from 2)),
+         ', '
+       ) AS actors
+FROM film
+JOIN film_actor USING (film_id)
+JOIN actor USING (actor_id)
+JOIN film_category USING (film_id)
+JOIN category USING (category_id)
+WHERE film.rating = 'G'
+  AND category.name = 'Documentary'
+GROUP BY film.title;
+
